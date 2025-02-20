30674d9a6347fe401229ead935f4902a6a8cbaa3
Peter Overeijnder <peterovereijnder@hotmail.com>
2025-02-20 01:31:28 +0100

    -

--- Engines/Unreal/Gvas/PropertyTypes.php
+++ Engines/Unreal/Gvas/PropertyTypes.php
@@ -3,24 +3,24 @@ namespace Php2Core\Gaming\Engines\Unreal\Gvas;
 
 enum PropertyTypes: string
 {
-	use \Php2Core\Collections\Enum\TInfo;
-	
-	case ArrayProperty = 'ArrayProperty';
-	case EnumProperty = 'EnumProperty';
-	case StructProperty = 'StructProperty';
-	case Guid = 'Guid';
-	case MapProperty = 'MapProperty';
-	case NameProperty = 'NameProperty';
-	case StrProperty = 'StrProperty';
-	case BoolProperty = 'BoolProperty';
-	case IntProperty = 'IntProperty';
-	case FloatProperty = 'FloatProperty';
-	case DateTime = 'DateTime';
-	case LinearColor = 'LinearColor';
-	case Vector = 'Vector';
-	case Quat = 'Quat';
-	case ByteProperty = 'ByteProperty';
-	case None = 'None';
-	case Int64Property = 'Int64Property';
-	case FixedPoint64 = 'FixedPoint64';
+    use \Php2Core\Collections\Enum\TInfo;
+
+    case ArrayProperty = 'ArrayProperty';
+    case EnumProperty = 'EnumProperty';
+    case StructProperty = 'StructProperty';
+    case Guid = 'Guid';
+    case MapProperty = 'MapProperty';
+    case NameProperty = 'NameProperty';
+    case StrProperty = 'StrProperty';
+    case BoolProperty = 'BoolProperty';
+    case IntProperty = 'IntProperty';
+    case FloatProperty = 'FloatProperty';
+    case DateTime = 'DateTime';
+    case LinearColor = 'LinearColor';
+    case Vector = 'Vector';
+    case Quat = 'Quat';
+    case ByteProperty = 'ByteProperty';
+    case None = 'None';
+    case Int64Property = 'Int64Property';
+    case FixedPoint64 = 'FixedPoint64';
 }
