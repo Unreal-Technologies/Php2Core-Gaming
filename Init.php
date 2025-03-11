2ce539702a4ba472017dce9f765065b007c2851d
Peter Overeijnder <peterovereijnder@hotmail.com>
2025-03-11 17:47:51 +0100

    -

--- Init.php
+++ Init.php
@@ -1,2 +1,2 @@
 <?php
-PHP2CORE -> get(\Php2Core\Php2Core\CoreProperties::Version) -> add(new \Php2Core\Version('Php2Core.Gaming', 1, 0, 0, 2, 'https://github.com/Unreal-Technologies/Php2Core-Gaming'));
+PHP2CORE -> get(\Php2Core\CoreProperties::Version) -> add(new \Php2Core\Version('Php2Core.Gaming', 1, 0, 0, 2, 'https://github.com/Unreal-Technologies/Php2Core-Gaming'));
